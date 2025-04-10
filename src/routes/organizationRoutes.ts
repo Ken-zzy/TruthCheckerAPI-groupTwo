217362d803add59804e2cc83e9703675279c35b4
Ken-zzy <nwakalokenechukwu@gmail.com>
2025-04-10 04:51:41 +0100

    April 10, 2025 4:51 AM

--- src/routes/organizationRoutes.ts
+++ src/routes/organizationRoutes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import organizationController from '../controllers/organizationController';
+const router = express.Router();
 import { tokenRequired } from '../middleware/authMiddleware';
 
-const router = express.Router();
 export const organizationRoutes = router;
 
 router.get('/', organizationController.getOrganizations);
